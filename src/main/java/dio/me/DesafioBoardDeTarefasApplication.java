9f61a7a1717b2365170c04329402b5538bd6c2d9
Caliel Passos <caliel1023@yahoo.com>
2025-03-24 12:19:31 -0300

    feat folder arcchitecture

---
+++ src/main/java/dio/me/DesafioBoardDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package dio.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBoardDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBoardDeTarefasApplication.class, args);
+	}
+
+}
